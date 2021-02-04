7d8ffbb834e4262fce4320d7e6e291bdfaa59152
John Temple <johntemple@Alenas-iMac.local>
2021-02-04 15:03:30 -0700

    footer completed and links up

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
-// alert('It works');
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  })
+})
